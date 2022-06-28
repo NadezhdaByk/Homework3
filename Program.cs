340947c92e4ffbf73d3811ab51b96a138bd37d30
Nadezhda <nbykova79@gmail.com>
2022-06-28 22:12:13 +0300

    сохранение

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// определение четного числа
+Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+if (n%2 > 0)
+{
+   Console.WriteLine("Число нечетное");
+}
+else
+{
+   Console.WriteLine("Число четное");
+}
